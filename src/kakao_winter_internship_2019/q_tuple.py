e4bb1f1014eff46eae619b21c9b531dcd9913af6
zeee1 <miw5281@gmail.com>
2020-04-24 21:23:03 +0900

    solve kakao winter intership 2019 - tuple

--- src/kakao_winter_internship_2019/q_tuple.py
+++ src/kakao_winter_internship_2019/q_tuple.py
@@ -1,7 +1,35 @@
 def solution(s):
     answer = []
 
+    s = s[1:len(s)-1]
+    splited = s.split("},")
+
+    s_list = list()
+    for i in splited:
+    	tmp_list = list()
+    	i = i[1:]
+
+    	split_by_comma = i.split(",")
+
+    	for c in split_by_comma:
+    		if "}" in c:
+    			c = c[:len(c)-1]
+    		tmp_list.append(int(c))
+    	s_list.append(tmp_list)
+
     
+    s_list.sort(key = len)
+
+    for i in range(0, len(s_list)):
+    	if i == 0:
+    		answer += s_list[i]
+    		continue
+
+    	diff = list(set(s_list[i])-set(s_list[i-1]))[0]
+    	answer.append(diff)
+
+
+
     return answer
 
 print(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"))
